e3749c3ee77151d063122a007388b5f3f5ad8026
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-09-24 15:07:22 +0200

    [init] Création du plugin

---
+++ twikin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Twikin
+ * Plugin URI: http://www.twikin.fr/
+ * Author: Fabien Quatravaux
+ * Author URI: http://fab1en.github.io
+ * Description: Insère des fiches de jeux provenant de Twikin dans la médiathèque
+ * Version: 1.0
+*/
